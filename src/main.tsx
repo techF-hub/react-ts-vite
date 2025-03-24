0b2c1db73d6d9e69e3427470d8d635be9f16e8aa
冯天保 <fengtianbao@i72.com>
2025-03-24 09:26:19 +0800

    fix: 修改配置

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "reset.css";
 import "@/styles/global.scss";
-import App from "./App.tsx";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
